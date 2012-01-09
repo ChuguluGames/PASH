560e59d9cc27e7dc2d00ea966575a3a3337ff957
Skunk <skunk@dreamwall.fr>
2012-01-09 13:17:59 +0100

    delete bin dir

--- android/bin/PASH-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/davidbillamboz/Projects/PASH/android/bin/PASH-debug-unaligned.apk : \
-/Users/davidbillamboz/Projects/PASH/android/bin/PASH.ap_ \
-/Users/davidbillamboz/Projects/PASH/android/bin/classes.dex \
-/Users/davidbillamboz/Projects/PASH/android/src/com/phonegap/pash/PASH.java-e \
-/Users/davidbillamboz/Projects/PASH/android/libs/phonegap-1.3.0.jar \
--- android/bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/Users/davidbillamboz/Projects/PASH/android/bin/classes.dex : \
-/Users/davidbillamboz/Projects/PASH/android/libs/phonegap-1.3.0.jar \
