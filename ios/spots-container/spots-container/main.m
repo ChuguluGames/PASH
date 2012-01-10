100414d659c6ab4ae9487d5d026dd64c70c2dd95
zomfg <sergio.kunats@gmail.com>
2012-01-10 17:27:49 +0100

    ios project

---
+++ ios/spots-container/spots-container/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  spots-container
+//
+//  Created by Sergio Kunats on 1/9/12.
+//  Copyright ChuguluGames 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
