d2ab751ce0f1100f199292afa12c3dcbacd65878
Skunk <skunk@dreamwall.fr>
2012-01-23 16:35:04 +0100

    fix android fastclick

--- brunch/vendor/scripts/jquery.fastclick.js
+++ brunch/vendor/scripts/jquery.fastclick.js
@@ -7,53 +7,36 @@ function activateFastClicks() {
 		// Special event definition.
 		$.event.special.click = {
 			setup: function() {
-				var self = this;
-				console.log ("fastclick")
-				// add fastbutton on the element if it doesnt exist yet
-				if($(self).data("fastbutton") === undefined) {
-					// console.log("add fclick to " + $(this).attr("id"))
-					$(self).data("fastbutton", new fastButton(self));
+				if(typeof $(this).data("fastclick_activated") === undefined || $(this).data("fastclick_activated") !== true) {
+
+					// listen to touchstart event
+					$(this).on('touchstart', function(event) {
+						// get the first touche
+						var touch = event.originalEvent.touches[0];
+
+						// dispatch click event
+						$(this).trigger({
+							type: 	"click",
+							pageX: 	touch.pageX,
+							pageY: 	touch.pageY
+						});
+					});
+
+					$(this).data("fastclick_activated", true);
 				}
 			},
 			teardown: function() {
-				var self = this;
-
 				// destroy fastbutton
-				$(self).unbind("touchstart")
-				$(self).data("fastbutton", null);
+				$(this).unbind("touchstart")
+				$(this).data("fastclick_activated", null);
 			}
 		};
 
 	})(jQuery);
 
-	$("a").on ("click", function(event) {
+	// activate the fastlinks
+	$("a").live ("click", function(event) {
+		console.log("link clicked")
 		return true;
 	});
 }
-
-/* Construct the FastButton with a reference to the element and click handler. */
-var fastButton = function (element) {
-	var self = this;
-
-	self.element = element;
-
-	if(element.addEventListener) {
-		element.addEventListener('touchstart', self, false);
-	}
-};
-
-/* acts as an event dispatcher */
-fastButton.prototype.handleEvent = function(event) {
-	console.log ("fastcliced")
-
-	event.preventDefault();
-	event.stopPropagation();
-
-	var dispatch = jQuery.Event("click");
-	dispatch.type = "click";
-	dispatch.pageX = event.touches[0].clientX;
-	dispatch.pageY = event.touches[0].clientY;
-	$(self.element).trigger(dispatch);
-
-	return false;
-};
